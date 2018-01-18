45c8a073335d1fcb6e2385e4527241b787706336
hardx4 <felipe_f0@hotmail.com>
2018-01-18 13:48:17 -0400

    Update HttpServer.cpp

--- src/Rpc/HttpServer.cpp
+++ src/Rpc/HttpServer.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2012-2016, The CryptoNote developers, The Bytecoin developers
 // Copyright (c) 2014-2016 XDN developers
+// Copyright (c) 2016 Karbowanec developers
 // Copyright (c) 2017 Niobio developers
 //
 // This file is part of Bytecoin.
