5bbe7cdeec95006cde903b74fdb5503d05a3c103
Helder Garcia <helder.garcia@gmail.com>
2018-03-23 10:36:36 -0300

    Change port for testnet.

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -107,8 +107,8 @@ const size_t   BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT        =  10000;  //by def
 const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  128;    //by default, blocks count in blocks downloading
 const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;
 
-const int      P2P_DEFAULT_PORT                              =  8313;
-const int      RPC_DEFAULT_PORT                              =  8314;
+const int      P2P_DEFAULT_PORT                              =  9000;
+const int      RPC_DEFAULT_PORT                              =  9001;
 
 const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000;
 const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
@@ -126,7 +126,8 @@ const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          //
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "";
 
 const char* const SEED_NODES[] = {
-	"testnet-nbr-01.niobioco.in:8313"
+	"testnet-nbr-01.niobioco.in:9000",
+	"remote-nbr-hydra.niobioco.in:9000"
 /*	"45.55.141.227:8313",
 	"138.197.222.188:8313",
 	"35.227.102.144:8313",
