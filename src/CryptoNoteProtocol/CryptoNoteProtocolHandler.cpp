377e24f65046aa450053f6b33b972d5619bf8636
Helder Garcia <helder.garcia@gmail.com>
2018-07-25 01:30:24 -0300

    Cleaner log messages.

--- src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
+++ src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
@@ -543,12 +543,8 @@ bool CryptoNoteProtocolHandler::on_connection_synchronized() {
   bool val_expected = false;
   if (m_synchronized.compare_exchange_strong(val_expected, true)) {
     logger(Logging::INFO) << ENDL << "**********************************************************************" << ENDL
-      << "You are now synchronized with the network. You may now start simplewallet." << ENDL
-      << ENDL
-      << "Please note, that the blockchain will be saved only after you quit the daemon with \"exit\" command or if you use \"save\" command." << ENDL
-      << "Otherwise, you will possibly need to synchronize the blockchain again." << ENDL
-      << ENDL
-      << "Use \"help\" command to see the list of available commands." << ENDL
+      << "You may now start simplewallet." << ENDL
+      << "To quit the daemon use \"exit\" command. Type \"help\" to see the list of all available commands." << ENDL
       << "**********************************************************************";
     m_core.on_synchronized();
     uint32_t height;
