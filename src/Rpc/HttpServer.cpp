fe7fe0d1784ca8f90f92b6d75d3220e65260197a
aiwe <maksymivv@gmail.com>
2018-08-01 16:22:51 +0300

    Move accept loop spawn to before a throw condition that stops it from accepting new connections.

--- src/Rpc/HttpServer.cpp
+++ src/Rpc/HttpServer.cpp
@@ -107,17 +107,17 @@ void HttpServer::acceptLoop() {
     BOOST_SCOPE_EXIT_ALL(this, &connection) {
       m_connections.erase(&connection); };
 
-    //auto addr = connection.getPeerAddressAndPort();
-		auto addr = std::pair<System::Ipv4Address, uint16_t>(static_cast<System::Ipv4Address>(0), 0);
-		try {
-			addr = connection.getPeerAddressAndPort();
-		} catch (std::runtime_error&) {
-			logger(WARNING) << "Could not get IP of connection";
-		}
+	workingContextGroup.spawn(std::bind(&HttpServer::acceptLoop, this));
+
+	//auto addr = connection.getPeerAddressAndPort();
+	auto addr = std::pair<System::Ipv4Address, uint16_t>(static_cast<System::Ipv4Address>(0), 0);
+	try {
+		addr = connection.getPeerAddressAndPort();
+	} catch (std::runtime_error&) {
+		logger(WARNING) << "Could not get IP of connection";
+	}
 
     logger(DEBUGGING) << "Incoming connection from " << addr.first.toDottedDecimal() << ":" << addr.second;
-
-    workingContextGroup.spawn(std::bind(&HttpServer::acceptLoop, this));
 
     System::TcpStreambuf streambuf(connection);
     std::iostream stream(&streambuf);
